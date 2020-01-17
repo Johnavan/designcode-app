f4c759e0bcc7431f3941948fd665a756ceae3b91
Johnavan Thomas <johnavanthomas@gmail.com>
2020-01-16 19:37:35 -0500

    update backgroundColor

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0F3F5',
     alignItems: 'center',
     justifyContent: 'center',
   },
