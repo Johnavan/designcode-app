b956206e8c44a266e82a9a1cbae7ea9be7253a64
Johnavan Thomas <johnavanthomas@gmail.com>
2020-01-16 20:19:41 -0500

    Working on styled components

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components";
+import { render } from "react-dom";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is a test run!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <TitleBar>
+          <Title>Welcome back,</Title>
+          <Name>Johnavan</Name>
+        </TitleBar>
+      </Container>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F3F5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.View`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.View`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 20px;
+`;
