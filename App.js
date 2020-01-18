d15f8695f0085742cad5dbba4e1b33fa6e53983a
Johnavan Thomas <johnavanthomas@gmail.com>
2020-01-17 21:25:39 -0500

    Working on card component

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Text } from "react-native";
 import styled from "styled-components";
-import { render } from "react-dom";
+import Card from "./components/Card";
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
         <TitleBar>
+          <Avatar source={require("./assets/avatar.jpg")} />
           <Title>Welcome back,</Title>
           <Name>Johnavan</Name>
         </TitleBar>
+        <Subtitle>Continue Learning</Subtitle>
+        <Card />
       </Container>
     );
   }
@@ -21,13 +24,13 @@ const Container = styled.View`
   background-color: #f0f3f5;
 `;
 
-const Title = styled.View`
+const Title = styled.Text`
   font-size: 16px;
   color: #b8bece;
   font-weight: 500;
 `;
 
-const Name = styled.View`
+const Name = styled.Text`
   font-size: 20px;
   color: #3c4560;
   font-weight: bold;
@@ -36,5 +39,25 @@ const Name = styled.View`
 const TitleBar = styled.View`
   width: 100%;
   margin-top: 50px;
-  padding-left: 20px;
+  padding-left: 80px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
 `;
