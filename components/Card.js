5c60e213d2aacabad4e3c8ae7592a31e22dfd17b
Johnavan Thomas <johnavanthomas@gmail.com>
2020-01-18 12:57:28 -0500

    finished cards

--- components/Card.js
+++ components/Card.js
@@ -7,6 +7,13 @@ const Card = props => (
       <Image source={require("../assets/background2.jpg")} />
       <Title>Styled Components</Title>
     </Cover>
+    <Content>
+      <Logo source={require("../assets/logo-react.png")} />
+      <Wrapper>
+        <Caption>React Native</Caption>
+        <Subtitle> 5 of 12 sections</Subtitle>
+      </Wrapper>
+    </Content>
   </Container>
 );
 
@@ -22,8 +29,55 @@ const Container = styled.View`
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
 `;
 
-const Cover = styled.View``;
+const Wrapper = styled.View`
+  margin-left: 10px;
+`;
 
-const Image = styled.Image``;
+const Content = styled.View`
+  padding-left: 20px;
+  flex-direction: row;
+  align-items: center;
+  height: 80px;
+`;
+const Logo = styled.Image`
+  width: 44px;
+  height: 44px;
+`;
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  text-transform: uppercase;
+  margin-top: 4px;
+`;
 
-const Title = styled.Text``;
+const Caption = styled.Text`
+  color: #3c4560;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Cover = styled.View`
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
+`;
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 20px;
+  width: 170px;
+`;
